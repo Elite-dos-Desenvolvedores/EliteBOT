a688de7ab140db9d08647806eb17a3e7cd3125a4
Gabriel Russo Vitagliano <gabrielrvita@gmail.com>
2019-10-02 18:49:43 -0300

    Space my channel command updated

--- comandos/staff/spacemychannel.js
+++ comandos/staff/spacemychannel.js
@@ -1,10 +1,10 @@
 exports.run = async(client, message, args) => {
-    const channelName = message.channel.name
+    const channelName = message.mentions.channels.first();
 
     if(!channelName.includes("-")) return message.reply('não é possivel colocar espaços nesse canal.').then(msg => msg.delete(5000));
     const newName = channelName.replace(/-/g, '\u2005');
 
-    message.channel.send(`${message.author}, você colocou espaços no canal . `).then(msg => msg.delete(5000));
+    message.channel.send(`${message.author}, você colocou espaços no canal. `).then(msg => msg.delete(5000));
     message.channel.setName(newName)
 
 }
