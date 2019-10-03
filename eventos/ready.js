b21e14bd772f7cff6dd930cfece732b5a0fd39fa
Gabriel Russo Vitagliano <gabrielrvita@gmail.com>
2019-10-03 14:49:53 -0300

    Rich presence loop add

--- eventos/ready.js
+++ eventos/ready.js
@@ -1,5 +1,7 @@
 exports.run = (client) => {
+    let gameloop = require(`./gameLoop.js`);
+    gameloop.run(client);
 
-const Discord = require("discord.js")
-console.log('log', `O Bot foi iniciado completamente com ${client.users.size} usuarios em ${client.guilds.size} servidores`)
+    const Discord = require("discord.js")
+    console.log('log', `O Bot foi iniciado completamente com ${client.users.size} usuarios em ${client.guilds.size} servidores`)
 }
