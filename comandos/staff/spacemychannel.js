244298a26b8e3397c30c7a3f4cca9021edc715ba
Gabriel Russo Vitagliano <gabrielrvita@gmail.com>
2019-10-03 12:47:16 -0300

    Spacemychannel command already working, updating permissions

--- comandos/staff/spacemychannel.js
+++ comandos/staff/spacemychannel.js
@@ -1,11 +1,12 @@
 exports.run = async(client, message, args) => {
+    if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`${message.author}, você não possui permissão para executar esse comando.`).then(msg => msg.delete(8000))
     const channelName = message.mentions.channels.first().name
 
     if(!channelName.includes("-")) return message.reply('não é possivel colocar espaços nesse canal.').then(msg => msg.delete(5000));
     const newName = channelName.replace(/-/g, '\u2005');
 
     message.channel.send(`${message.author}, você colocou espaços no canal. `).then(msg => msg.delete(5000));
-    message.mentions.channels.first().setName(newName)
+    await message.mentions.channels.first().setName(newName)
 
 }
 
