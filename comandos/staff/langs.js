04ef8848486544ba4055206ec2416e201a16f142
Gabriel Russo Vitagliano <gabrielrvita@gmail.com>
2019-10-03 14:33:07 -0300

    Reaction roles update

--- comandos/staff/langs.js
+++ comandos/staff/langs.js
@@ -41,6 +41,7 @@ exports.run = async (client, message, args) => {
        `)
         .setColor('RANDOM')
         .setTimestamp();
+        🇦 🇧 🇨 🇩 🇪 🇫 🇬 🇭 🇮 🇯 🇰 🇱 🇲 🇳 🇴 🇵 🇶 🇷 🇸 🇹 🇺 🇻 🇼 🇽 🇾 🇿
     message.channel.send(embed).then(async msg => {
 
         await msg.react('🇦');
@@ -59,10 +60,152 @@ exports.run = async (client, message, args) => {
             max: 10,
             time: 30000,
             errors: ['time']
+        }).then(collected => {
 
-        })
-    })
-}
+            const reaction = collected.first();
+
+            switch (reaction.emoji.name) {
+                case '😀':
+                    if (message.member.roles.has(a.id)) {
+                        
+                        return message.channel.send('Você já tem essa tag!').then(m => m.delete(3000));
+                    }
+                    message.member.addRole(a).catch(err => {
+                        console.log(err);
+                        return message.channel.send(`Erro ao te adicionar a tag: **${err.message}**.`);
+                    });
+                    message.channel.send(`Você recebeu a tag: **${a.name}**!`).then(m => m.delete(3000));
+                    
+                    return;
+                case '😁':
+                    if (message.member.roles.has(b.id)) {
+                        
+                        return message.channel.send('Você já tem essa tag!').then(m => m.delete(3000));
+                    }
+                    message.member.addRole(b).catch(err => {
+                        console.log(err);
+                        return message.channel.send(`Erro ao te adicionar a tag: **${err.message}**.`);
+                    });
+                    message.channel.send(`Você recebeu a tag: **${b.name}**!`).then(m => m.delete(3000));
+                    
+                    return;
+                case '😂':
+                    if (message.member.roles.has(c.id)) {
+                        
+                        return message.channel.send('Você já tem essa tag!').then(m => m.delete(3000));
+                    }
+                    message.member.addRole(c).catch(err => {
+                        console.log(err);
+                        return message.channel.send(`Erro ao te adicionar a tag: **${err.message}**.`);
+                    });
+                    message.channel.send(`Você recebeu a tag: **${c.name}**!`).then(m => m.delete(3000));
+                    
+                    return;
+                case '🤣':
+                    if (message.member.roles.has(d.id)) {
+                        
+                        return message.channel.send('Você já tem essa tag!').then(m => m.delete(3000));
+                    }
+                    message.member.addRole(d).catch(err => {
+                        console.log(err);
+                        return message.channel.send(`Erro ao te adicionar a tag: **${err.message}**.`);
+                    });
+                    message.channel.send(`Você recebeu a tag: **${d.name}**!`).then(m => m.delete(3000));
+                    
+                    return;
+                case '😃':
+                    if (message.member.roles.has(e.id)) {
+                        
+                        return message.channel.send('Você já tem essa tag!').then(m => m.delete(3000));
+                    }
+                    message.member.addRole(e).catch(err => {
+                        console.log(err);
+                        return message.channel.send(`Erro ao te adicionar a tag: **${err.message}**.`);
+                    });
+                    message.channel.send(`Você recebeu a tag: **${e.name}**!`).then(m => m.delete(3000));
+                    
+                    return;
+                case '😄':
+                    if (message.member.roles.has(f.id)) {
+                        
+                        return message.channel.send('Você já tem essa tag!').then(m => m.delete(3000));
+                    }
+                    message.member.addRole(f).catch(err => {
+                        console.log(err);
+                        return message.channel.send(`Erro ao te adicionar a tag: **${err.message}**.`);
+                    });
+                    message.channel.send(`Você recebeu a tag: **${f.name}**!`).then(m => m.delete(3000));
+                    
+                    return;
+                case '😅':
+                    if (message.member.roles.has(g.id)) {
+                        
+                        return message.channel.send('Você já tem essa tag!').then(m => m.delete(3000));
+                    }
+                    message.member.addRole(g).catch(err => {
+                        console.log(err);
+                        return message.channel.send(`Erro ao te adicionar a tag: **${err.message}**.`);
+                    });
+                    message.channel.send(`Você recebeu a tag: **${g.name}**!`).then(m => m.delete(3000));
+                    
+                    return;
+                case '😆':
+                    if (message.member.roles.has(h.id)) {
+                        
+                        return message.channel.send('Você já tem essa tag!').then(m => m.delete(3000));
+                    }
+                    message.member.addRole(h).catch(err => {
+                        console.log(err);
+                        return message.channel.send(`Erro ao te adicionar a tag: **${err.message}**.`);
+                    });
+                    message.channel.send(`Você recebeu a tag: **${h.name}**!`).then(m => m.delete(3000));
+                    
+                    return;
+                case '😉':
+                    if (message.member.roles.has(i.id)) {
+                        
+                        return message.channel.send('Você já tem essa tag!').then(m => m.delete(3000));
+                    }
+                    message.member.addRole(i).catch(err => {
+                        console.log(err);
+                        return message.channel.send(`Erro ao te adicionar a tag: **${err.message}**.`);
+                    });
+                    message.channel.send(`Você recebeu a tag: **${i.name}**!`).then(m => m.delete(3000));
+                    
+                    return;
+                case '😊':
+                    if (message.member.roles.has(j.id)) {
+                        
+                        return message.channel.send('Você já tem essa tag!').then(m => m.delete(3000));
+                    }
+                    message.member.addRole(j).catch(err => {
+                        console.log(err);
+                        return message.channel.send(`Erro ao te adicionar a tag: **${err.message}**.`);
+                    });
+                    message.channel.send(`Você recebeu a tag: **${j.name}**!`).then(m => m.delete(3000));
+                    
+                    return;
+                case '😋':
+                    if (message.member.roles.has(k.id)) {
+                        
+                        return message.channel.send('Você já tem essa tag!').then(m => m.delete(3000));
+                    }
+                    message.member.addRole(k).catch(err => {
+                        console.log(err);
+                        return message.channel.send(`Erro ao te adicionar a tag: **${err.message}**.`);
+                    });
+                    message.channel.send(`Você recebeu a tag: **${k.name}**!`).then(m => m.delete(3000));
+                    
+                    return;
+
+            }
+        }).catch(collected => {
+            return message.channel.send(`Não consegui adicionar essa tag para você!`);
+        });
+
+    });
+
+};
 
 exports.help = {
     name: 'welcomeroles'
