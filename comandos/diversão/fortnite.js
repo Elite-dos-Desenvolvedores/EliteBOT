3d27196519df560b959d91e4495a166d57f0def2
Gabriel Russo Vitagliano <gabrielrvita@gmail.com>
2019-10-03 23:22:39 -0300

    Fortnite status command updated

--- comandos/diversão/fortnite.js
+++ comandos/diversão/fortnite.js
@@ -1,50 +1,77 @@
-const Discord = require("discord.js");
-const Fortnite = require("fortnite");
-const fortnite = require("../config.json")
-const ft = new Fortnite(fortnite.fortnite)
+const FortClient = require('fortnite')
+const fortnite = new FortClient('../config.json')
 
-module.exports.run = async (data, message, args) => {
+exports.run = ({ client, message, args, lang, language }, t) => {
+  let razaou = args.slice(0).join(' ')
+  let razaod = args.slice(1).join(' ')
 
-    let username = args.join(` `)
-    if(!username) return message.channel.send("Insira o nome de um jogador para ver seus status.")
-    let platform = args[1];
+  if (!razaou.length < 1) {
+    var plataformas = ['xbl', 'pc', 'ps4']
+    var plataforma
 
-    let data = ft.getInfo(username).then(data => {
+    if (plataformas.includes(args[0].toLowerCase())) {
+      plataforma = args[0].toLowerCase()
 
-        const url = 'https://i.imgur.com/y29m0wF.png';
-
-        let stats = data.lifetimeStats;
-        let kills = stats.find(s => s.stat == "kills");
-        let wins = stats.find(s => s.stat == "wins");
-        let top5s = stats.find(s => s.stat == "top5s");
-        let kd = stats.find(s => s.stat == "kd");
-        let mPlayed = stats.find(s => s.stat == "matchesPlayed");
-        let top3 = stats.find(s => s.stat == "top3");
-        let score = stats.find(s => s.stat == "score");
-        let tPlayed = stats.find(s => s.stat == "timePlayed");
-
-        let embed = new Discord.RichEmbed()
-        .setTitle(`Status de ${data.username} no Krunker`)
-        .setThumbnail(url)
-        .setColor("RANDOM")
-        .addField("Kills", kills.value, true)
-        .addField("Vitórias", wins.value, true)
-        .addField("KDR", kd.value, true)
-        .addField("Top 3", top3.value, true)
-        .addField("Partidas Jogadas", mPlayed.value, true)
-        .addField("Tempo jogado", tPlayed.value, true)
-        .setFooter(`Score de ${data.username} é: ${score.value}`)
-        .setTimestamp();
-
-        message.channel.send(embed);
-
-    }).catch(e => {
-        console.log(e)
-        message.channel.send("Não foi possível encontrar um usuário com esse nick.");
-    });
-
-}
-
-module.exports.help = {
-  name: "fortnite"
+      if (!razaod.length < 1) {
+        fortnite.user(`${args[1]}`, plataforma).then(usuario => {
+          message.channel.sendMessage({
+            'embed': {
+              'color': 11676858,
+              'timestamp': new Date(),
+              'footer': {
+                'icon_url': message.author.displayAvatarURL,
+                'text': message.author.username
+              },
+              'thumbnail': {
+                'url': 'https://i.imgur.com/ZjtLNE9.jpg'
+              },
+              'author': {
+                'name': `${usuario.username}`,
+                'icon_url': 'https://i.imgur.com/NkIThyN.jpg'
+              },
+              'fields': [{
+                'name': ':notepad_spiral: Username:',
+                'value': `${usuario.username}`,
+                'inline': true
+              },
+              {
+                'name': ':video_game: Plataforma:',
+                'value': `${usuario.platform}`,
+                'inline': true
+              },
+              {
+                'name': ':sunglasses: Solo:',
+                'value': `**Kd:** ${usuario.stats.solo.kd}\n**Pontuação:** ${Number(usuario.stats.solo.score).toLocaleString()}\n**Partidas:** ${Number(usuario.stats.solo.matches).toLocaleString()}\n**Vitórias:** ${Number(usuario.stats.solo.wins).toLocaleString()}`,
+                'inline': true
+              },
+              {
+                'name': ':handshake: Duo:',
+                'value': `**Kd:** ${usuario.stats.duo.kd}\n**Pontuação:** ${Number(usuario.stats.duo.score).toLocaleString()}\n**Partidas:** ${Number(usuario.stats.duo.matches).toLocaleString()}\n**Vitórias:** ${Number(usuario.stats.duo.wins).toLocaleString()}`,
+                'inline': true
+              },
+              {
+                'name': ':fingers_crossed: Squad:',
+                'value': `**Kd:** ${usuario.stats.squad.kd}\n**Pontuação:** ${Number(usuario.stats.squad.score).toLocaleString()}\n**Partidas:** ${Number(usuario.stats.squad.matches).toLocaleString()}\n**Vitórias:** ${Number(usuario.stats.squad.wins).toLocaleString()}`,
+                'inline': true
+              },
+              {
+                'name': ':trophy: Total:',
+                'value': `**Kd:** ${(usuario.stats.solo.kd + usuario.stats.duo.kd + usuario.stats.squad.kd)}\n**Pontuação:** ${Number(usuario.stats.solo.score + usuario.stats.duo.score + usuario.stats.squad.score).toLocaleString()}\n**Partidas:** ${Number(usuario.stats.solo.matches + usuario.stats.duo.matches + usuario.stats.squad.matches).toLocaleString()}\n**Vitórias:** ${Number(usuario.stats.solo.wins + usuario.stats.duo.wins + usuario.stats.squad.wins).toLocaleString()}`,
+                'inline': true
+              }
+              ]
+            }
+          })
+        }).catch(err => {
+          message.channel.sendMessage(':x: **Usuário não encontrado.**')
+        })
+      } else {
+        message.channel.sendMessage(':x: **Diga o username da conta.**')
+      }
+    } else {
+      message.channel.sendMessage(`:x: **Plataforma inválida:** \`${plataformas.join('` **|** `')}\``)
+    }
+  } else {
+    message.channel.sendMessage(':x: **Diga a plataforma da conta.**')
+  }
 }
