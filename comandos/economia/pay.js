b94d93c2da1c54761eeda0bbee3473e1f2a64317
Gabriel Russo Vitagliano <gabrielrvita@gmail.com>
2019-10-06 16:53:32 -0300

    Testing pay command again

--- comandos/economia/pay.js
+++ comandos/economia/pay.js
@@ -1,29 +1,32 @@
 const Discord = require("discord.js")
 
-exports.run = async (args, message) => {
-  let member = message.mentions.users.first() || message.guild.members.get(args[0]).user
-  if (!member) return message.reply("mencione um usuário para enviar um pagamento.")
-  if (member.id === message.author.id) return message.reply("você não pode fazer um pagamento para você mesmo!")
-  let value = args[1]
-  if (!parseInt(value)) return message.reply("insira um valor númerico.")
-  if (parseInt(value) < 0) return message.reply('valor de coins inválido, insira um valor válido');
+exports.run = async (client, message, args) => {
+  const valor = parseInt(args[1]);
+
+  let member = message.mentions.users.first();
+  if (!member) return message.reply("mencione um usuário para enviar um pagamento.");
+  if (member.id === message.author.id) return message.reply("você não pode fazer um pagamento para você mesmo!");
+  if(isNaN(args[1])) return message.reply('valor de coins inválido, insira um valor válido');
+  
 
   let doador = await database.Users.findOne({
     '_id': message.author.id
-  })
-  if (doador.coins < value) return message.reply("você não tem coins suficientes para isso!")
+  });
+
+  if (doador.coins < valor) return message.reply("você não tem coins suficientes para isso!");
+
+
+
   let membro = await database.Users.findOne({
     '_id': member.id
-  })
-  doador.coins -= Number(value)
-  membro.coins += Number(value)
-  membro.save()
-  doador.save()
+  });
 
-  message.reply("coins enviado com sucesso", {
-    member: member,
-    value: value.toLocaleString()
-  })
+  doador.coins -= valor
+  membro.coins += valor
+  membro.save();
+  doador.save();
+
+  await message.channel.send(`${message.author} você enviou **${valor}** coins para ${member} com sucesso!`);
 }
 
 exports.help = {
