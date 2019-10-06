3d72531ad4bcfdf0554d456a260164f1a9071b17
Gabriel Russo Vitagliano <gabrielrvita@gmail.com>
2019-10-06 16:50:20 -0300

    Refactoring again pay command

--- comandos/economia/pay.js
+++ comandos/economia/pay.js
@@ -4,25 +4,19 @@ exports.run = async (args, message) => {
   let member = message.mentions.users.first() || message.guild.members.get(args[0]).user
   if (!member) return message.reply("mencione um usuário para enviar um pagamento.")
   if (member.id === message.author.id) return message.reply("você não pode fazer um pagamento para você mesmo!")
-  if (isNaN(args[1])) return message.reply('valor de coins inválido, insira um valor válido');
-
+  let value = args[1]
+  if (!parseInt(value)) return message.reply("insira um valor númerico.")
+  if (parseInt(value) < 0) return message.reply('valor de coins inválido, insira um valor válido');
 
   let doador = await database.Users.findOne({
     '_id': message.author.id
   })
-
   if (doador.coins < value) return message.reply("você não tem coins suficientes para isso!")
-
-
-
   let membro = await database.Users.findOne({
     '_id': member.id
   })
-
-
-  const valor = parseInt(args[1])
-  doador.coins -= valor
-  membro.coins += valor
+  doador.coins -= Number(value)
+  membro.coins += Number(value)
   membro.save()
   doador.save()
 
