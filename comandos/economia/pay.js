93326fc87565619d5a08e3b9a26ccad079f2da6a
Gabriel Russo Vitagliano <gabrielrvita@gmail.com>
2019-10-06 14:50:03 -0300

    Pay command fixed

--- comandos/economia/pay.js
+++ comandos/economia/pay.js
@@ -5,7 +5,7 @@ exports.run = async (args, message) => {
   if (!member) return message.reply("mencione um usuário para enviar um pagamento.")
   if (member.id === message.author.id) return message.reply("você não pode fazer um pagamento para você mesmo!")
   let value = args[1]
-  if (!value) return message.reply("insira um valor númerico.")
+  if (Number.isNaN(value)) return message.reply("insira um valor númerico.")
   let invalidValue = Number(value) < 0 || Number(value) === Infinity || isNaN(value)
   if (invalidValue) return message.reply("valor de coins invalido, insira um valor valido.")
   let doador = await database.Users.findOne({
