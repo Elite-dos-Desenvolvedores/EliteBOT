bd78b34cda91269eee835de4283a97c4e41c6ad8
Gabriel Russo Vitagliano <gabrielrvita@gmail.com>
2019-10-06 16:20:54 -0300

    Refactoring pay command

--- comandos/economia/pay.js
+++ comandos/economia/pay.js
@@ -1,30 +1,28 @@
 const Discord = require("discord.js")
 
 exports.run = async (args, message) => {
-  
-  let args = message.content.split(" ").slice(1)
-
   let member = message.mentions.users.first() || message.guild.members.get(args[0]).user
   if (!member) return message.reply("mencione um usuário para enviar um pagamento.")
   if (member.id === message.author.id) return message.reply("você não pode fazer um pagamento para você mesmo!")
-  let value = args[1]
-  console.log(args[0])
-  console.log(args[1])
-  console.log(value)
-  console.log(Number(value))
-  console.log(parseInt(value))
-  if (Number.isNaN(value)) return message.reply("insira um valor númerico.")
-  if (isNaN(value) || Number.parseInt(value) < 0) return message.reply('valor de coins inválido, insira um valor válido');
+  if (isNaN(args[1])) return message.reply('valor de coins inválido, insira um valor válido');
+
 
   let doador = await database.Users.findOne({
     '_id': message.author.id
   })
+
   if (doador.coins < value) return message.reply("você não tem coins suficientes para isso!")
+
+
+
   let membro = await database.Users.findOne({
     '_id': member.id
   })
-  doador.coins -= Number(value)
-  membro.coins += Number(value)
+
+
+  const valor = args[1]
+  doador.coins -= valor
+  membro.coins += valor
   membro.save()
   doador.save()
 
