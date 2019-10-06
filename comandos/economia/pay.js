77fa173459ec6dd4786c75fee5d434839ceb57d1
Gabriel Russo Vitagliano <gabrielrvita@gmail.com>
2019-10-06 17:03:04 -0300

    Updating pharases on pay command

--- comandos/economia/pay.js
+++ comandos/economia/pay.js
@@ -8,11 +8,11 @@ exports.run = async (args, message) => {
   if (!!parseInt(value)) return message.reply("insira um valor númerico.")
   if (parseInt(value) < 0) return message.reply('valor de coins inválido, insira um valor válido');
 
-  let doador = await database.Users.findOne({
+  let doador = await Database.Users.findOne({
     '_id': message.author.id
   })
   if (doador.coins < value) return message.reply("você não tem coins suficientes para isso!")
-  let membro = await database.Users.findOne({
+  let membro = await Database.Users.findOne({
     '_id': member.id
   })
   doador.coins -= Number(value)
@@ -20,10 +20,7 @@ exports.run = async (args, message) => {
   membro.save()
   doador.save()
 
-  message.reply("coins enviado com sucesso", {
-    member: member,
-    value: value.toLocaleString()
-  })
+  await message.channel.send(`${message.author} você enviou **${valor}** para ${member} com sucesso.`);
 }
 
 exports.help = {
