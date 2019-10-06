e3625cddcaaca2741b674f9a7aa1d84447714255
Gabriel Russo Vitagliano <gabrielrvita@gmail.com>
2019-10-06 17:31:00 -0300

    Refactoring again pay command

--- comandos/economia/pay.js
+++ comandos/economia/pay.js
@@ -1,27 +1,30 @@
 const Discord = require("discord.js")
 const Database = require('../../database.js')
 
-exports.run = async (args, message) => {
-  let member = message.mentions.users.first() || message.guild.members.get(args[0]).user
-  if (!member) return message.reply("mencione um usuário para enviar um pagamento.")
-  if (member.id === message.author.id) return message.reply("você não pode fazer um pagamento para você mesmo!")
-  let value = args[1]
-  if (!!parseInt(value)) return message.reply("insira um valor númerico.")
-  if (parseInt(value) < 0) return message.reply('valor de coins inválido, insira um valor válido');
+exports.run = async (client, message, args) => {
+  const valor = parseInt(args[1]);
 
-  let doador = await Database.Users.findOne({
+  let member = message.mentions.users.first();
+  if (!member) return message.reply("mencione um usuário para enviar um pagamento.");
+  if (member.id === message.author.id) return message.reply("você não pode fazer um pagamento para você mesmo!");
+  if (isNaN(args[1])) return message.reply('valor de coins inválido, insira um valor válido');
+  let doador = await database.Users.findOne({
     '_id': message.author.id
-  })
-  if (doador.coins < value) return message.reply("você não tem coins suficientes para isso!")
-  let membro = await Database.Users.findOne({
-    '_id': member.id
-  })
-  doador.coins -= parseInt(value)
-  membro.coins += parseInt(value)
-  membro.save()
-  doador.save()
+  });
 
-  await message.channel.send(`${message.author} você enviou **${value}** para ${member} com sucesso.`);
+  if (doador.coins < valor) return message.reply("você não tem coins suficientes para isso!");
+
+  let membro = await database.Users.findOne({
+    '_id': member.id
+  });
+
+  const valor = parseInt(args[1]);
+  doador.coins -= valor
+  membro.coins += valor
+  membro.save();
+  doador.save();
+
+  await message.channel.send(`${message.author} você enviou **${valor}** para ${member} com sucesso.`);
 }
 
 exports.help = {
