edd9d9c6723e8b47d8fe46947eb573407558cdd6
Gabriel Russo Vitagliano <gabrielrvita@gmail.com>
2019-10-06 17:33:17 -0300

    Defining database on pay command

--- comandos/economia/pay.js
+++ comandos/economia/pay.js
@@ -2,21 +2,23 @@ const Discord = require("discord.js")
 const Database = require('../../database.js')
 
 exports.run = async (client, message, args) => {
+  
+  const valor = parseInt(args[1]);
+  
   let member = message.mentions.users.first();
   if (!member) return message.reply("mencione um usuário para enviar um pagamento.");
   if (member.id === message.author.id) return message.reply("você não pode fazer um pagamento para você mesmo!");
   if (isNaN(args[1])) return message.reply('valor de coins inválido, insira um valor válido');
-  let doador = await database.Users.findOne({
+  let doador = await Databae.Users.findOne({
     '_id': message.author.id
   });
 
   if (doador.coins < valor) return message.reply("você não tem coins suficientes para isso!");
 
-  let membro = await database.Users.findOne({
+  let membro = await Database.Users.findOne({
     '_id': member.id
   });
 
-  const valor = parseInt(args[1]);
   doador.coins -= valor
   membro.coins += valor
   membro.save();
