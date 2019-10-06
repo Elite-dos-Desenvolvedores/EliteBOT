bb25ae2d406d84ed9b343a7af4cea83ec68cbac1
Gabriel Russo Vitagliano <gabrielrvita@gmail.com>
2019-10-06 13:56:37 -0300

    Fixing bugs on pay command

--- comandos/economia/pay.js
+++ comandos/economia/pay.js
@@ -1,24 +1,24 @@
 const Discord = require("discord.js")
 
 exports.run = async (message, args) => {
-  let member = message.mentions.users.first() || users.get(args[0])
+  let member = message.mentions.users.first() || client.users.get(args[0])
   if (!member) return message.reply("mencione um usuário para enviar um pagamento.")
   if (member.id === message.author.id) return message.reply("você não pode fazer um pagamento para você mesmo!")
   let value = args[1]
   if (!value) return message.reply("insira um valor númerico.")
   let invalidValue = Number(value) < 0 || Number(value) === Infinity || isNaN(value)
   if (invalidValue) return message.reply("valor de coins invalido, insira um valor valido.")
-  let donator = await database.Users.findOne({
+  let doador = await database.Users.findOne({
     '_id': message.author.id
   })
-  if (donator.coins < value) return message.reply("você não tem coins suficientes para isso!")
+  if (doador.coins < value) return message.reply("você não tem coins suficientes para isso!")
   let membro = await database.Users.findOne({
     '_id': member.id
   })
-  donator.coins -= Number(value)
+  doador.coins -= Number(value)
   membro.coins += Number(value)
   membro.save()
-  donator.save()
+  doador.save()
 
   message.reply("coins enviado com sucesso", {
     member: member,
