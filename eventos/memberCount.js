20865624729a1d33f0be3fb6ef284dbb18f70a4c
Bruno Martins <bruno.martins1998@hotmail.com>
2019-10-07 21:18:36 +0100

    Move `gameLoop.js` to utils folder

--- eventos/memberCount.js
+++ eventos/memberCount.js
@@ -1,12 +1,3 @@
-exports.name = 'ready';
-exports.run = (client) => {
-    let gameloop = require(`./gameLoop.js`);
-    gameloop.run(client);
-
-    const Discord = require("discord.js")
-    console.log('log', `O Bot foi iniciado completamente com ${client.users.size} usuarios em ${client.guilds.size} servidores`)
-}
-
 module.exports = [
     {
         name: 'guildMemberAdd',
--- eventos/ready.js
+++ eventos/ready.js
@@ -1,8 +1,8 @@
+const gameLoop = require('../utils/gameLoop');
+
 exports.name = 'ready';
 exports.run = (client) => {
-    let gameloop = require(`./gameLoop.js`);
-    gameloop.run(client);
+    gameLoop.run(client);
 
-    const Discord = require("discord.js")
-    console.log('log', `O Bot foi iniciado completamente com ${client.users.size} usuarios em ${client.guilds.size} servidores`)
-}
+    console.log('log', `O Bot foi iniciado completamente com ${client.users.size} usuarios em ${client.guilds.size} servidores`);
+};
