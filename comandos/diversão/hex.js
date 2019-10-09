69ecdc9ffc2de127a9745e7f39efd074bba48296
Gabriel Russo Vitagliano <gabrielrvita@gmail.com>
2019-10-09 12:06:26 -0300

    Fixing hashtag error on color command

--- comandos/diversão/hex.js
+++ comandos/diversão/hex.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 
 const validate = (color) => {
     if (!color || typeof color !== 'string') return false;
+    color = color.replace('', '#');
 
     switch (color.length) {
         case 3:
@@ -17,7 +18,7 @@ const validate = (color) => {
 
 exports.run = async (client, message, args) => {
 
-    let hex = args.join(' ');
+    let hex = args.join(' ').replace('', '#');
     let r = Number(hex.substring(0, 2), 16);
     let g = parseInt(hex.substring(2, 4), 16);
     let b = parseInt(hex.substring(4, 6), 16);
